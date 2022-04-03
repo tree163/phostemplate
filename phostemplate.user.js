d07840d772da4f1efa68f7f5d8cf37c7c5ff39d0
tree163 <102902182+tree163@users.noreply.github.com>
2022-04-03 05:47:02 -0500

    Update phostemplate.user.js

--- phostemplate.user.js
+++ phostemplate.user.js
@@ -15,7 +15,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://cdn.discordapp.com/attachments/894072123172225034/960122825245601792/phostemplate.png";
+            i.src = "https://media.discordapp.net/attachments/894072123172225034/960128227869876244/phostemplate.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
             console.log(i);
             return i;
